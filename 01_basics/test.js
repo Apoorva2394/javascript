8d6fe2955e369a1712f5a9d412f228ebb40282b5
Apoorva gupta <125957503+Apoorva2394@users.noreply.github.com>
2024-05-02 09:50:28 +0000

    string

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,33 @@
 console.log("apoorva")
+"use strict"; // treat all JS code as newer version
+// null => standalone value does not point any object 
+// undefined => declared but not assigned a value
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+// to fetch data
+const name = "hitesh"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+//to declare number  or any other dATA
+const balance = new Number(100)
+const gameName = new String('hitesh-hc-com')
+console.log(gameName[2]);
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+console.log(gameName.__proto__);//exposes the [[Prototype]] (either an object or null ) of this object
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
